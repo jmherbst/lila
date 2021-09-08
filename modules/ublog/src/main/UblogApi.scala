f244af361a353b8c117b6dc81efd9f36a58babb8
Thibault Duplessis <t@lichess.org>
2021-09-08 14:21:01 +0200

    ublog tweaks

--- modules/ublog/src/main/UblogApi.scala
+++ modules/ublog/src/main/UblogApi.scala
@@ -91,7 +91,7 @@ final class UblogApi(
       id = post.id.value,
       slug = post.slug,
       title = post.title,
-      imageUrl = UblogPost.thumbnail(picfitUrl, imageId, _.Large)
+      imageUrl = UblogPost.thumbnail(picfitUrl, imageId, _.Small)
     )
   }
 
